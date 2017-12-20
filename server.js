1bef8dfcf1dc81dce92f30f7071cc1f68dfaafb4
Sam Gould <sjg326@gmail.com>
2017-12-20 12:17:31 -0800

    server.js functionality built

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require(express);
 const morgan = require('morgan');
 
 const app = express();
+const blogRouter = require('/blogRouter');
 
 app.use(morgan('common'));
 
@@ -13,3 +14,8 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.use('/blog-posts', blogRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening at port ${process.env.PORT || 8080}`);
+});
