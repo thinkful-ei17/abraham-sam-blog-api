658b27869b6d7ecc656e83ed0ceb8e6d10fd3a7c
Abraham Cuenca <websiteprojectmanager@gmail.com>
2017-12-20 13:35:03 -0800

    Add PUT method to Blog API

--- blogRouter.js
+++ blogRouter.js
@@ -8,6 +8,8 @@ const jsonParser = bodyParser.json();
 
 const {BlogPosts} = require('./models');
 
+BlogPosts.create('Hello World!', 'My first post', 'Thinkful Student');
+
 router.get('/', (req, res) => {
   res.json(BlogPosts.get());
 });
@@ -30,6 +32,35 @@ router.post('/', jsonParser, (req, res) => {
 router.delete('/:id', (req, res) => {
   console.log(`Deleted post with id ${req.params.id}`);
   BlogPosts.delete(req.params.id);
+  res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for(let i=0; i<requiredFields.length; i++){
+    const field = requiredFields[i];
+    if(!(field in req.body)){
+      const message = `Missing ${field} from blog post input`;
+      console.error(message);
+      res.status(400).send(message);
+    }
+  }
+
+  if(req.params.id !== req.body.id){
+    const message = `Request Id ${req.params.id} did not match body id ${req.body.id}`;
+    console.error(message);
+    res.status(400).send(message);
+  }
+  const  {title, author, content, publishDate, id}  = req.body;
+  const updatedObject = {
+    id,
+    title, 
+    author, 
+    content, 
+    publishDate
+  };
+
+  BlogPosts.update(updatedObject);
   res.status(204).end();
 });
 
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(morgan('common'));
 
 app.use(express.static('public'));
 
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
